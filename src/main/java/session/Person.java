d8c3d59273b3ef868ddaabd070aae5587e3adde8
Nikta Zemlianskikh <rymzar@gmail.com>
2023-04-18 22:06:52 +0300

    lesson Cookie

---
+++ src/main/java/session/Person.java
@@ -0,0 +1,12 @@
+package session;
+
+class Person{
+    String name;
+    int age;
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    String getName() { return name;}
+    int getAge() {return age;}
+}
